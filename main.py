5cbd5e16ddfb5955e5c3fb3b79971584912d244d
Nstapp <jfuglifyli@>
2023-11-25 19:28:04 +0300

    ещё что-то добавили

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import requests
 
 # This is a sample Python script.
 
-
-
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -13,9 +9,3 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-
-
-
-
-
